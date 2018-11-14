77a1c75bbeb161a01e970cbcdbe9aa7d67a13819
willgreenfield <34075404+willgreenfield@users.noreply.github.com>
2018-11-14 03:02:24 -0600

    README update + more
    README file now contains step-by-step instructions to build the laser tower, some picture files were re-named, and the repository was organized.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # NinjaCat-LaserTower
+Ninja Cat is a robot laser toy that randomly moves a servo tower on the X and Y
+axis and lights up an attached laser to train your cat to be a ninja.
+This project is powered by an Arduino Uno.
+## The Build
+Follow these instructions to get your cat training in no time!
+1. Assemble the pan/tilt servo module:
+
+  - [Pan/Tilt Assembly](https://learn.adafruit.com/mini-pan-tilt-kit-assembly/getting-started) - by Adafruit
+
+
+2. The Altoids tin:
+  - Cut out a port to fit the Arduino's USB connector
+  - Cut out a slot for servo and laser wires
+  - Mount the pan/tilt module to the lid of the tin
+
+
+3. Wiring:
+
+  - Connect the laser to ground and pin 13
+  - 5v to x-servo and y-servo in series
+  - Ground (GND) to x-servo and y-servo in series
+  - x-servo to pin 6
+  - y-servo to pin 9
+
+![Wiring Diagram](pictures/NinjaCat-Wiring.png)
+
+4. The program:
+  - Open laserTower.ino in the Arduino IDE or [use the online editor](https://create.arduino.cc/editor)
+  - Upload the program to the board
+  - Modify these values based on the size of your ninja training grounds and fitness level of your cat
+  ```c
+  #define MIN_X 10        // X-servo angle range [MIN_X, MAX_X]
+  #define MAX_X 90      
+  #define MIN_Y 0         // Y-servo angle range [MIN_Y, MAX_Y]
+  #define MAX_Y 40
+  #define MIN_MOVE 10      // smallest move distance allowed
+  #define MIN_FREEZE 100   // delay between moves range [MIN_FREEZE, MAX_FREEZE]
+  #define MAX_FREEZE 1500
+  #define MIN_MOVE_TIME 20 // time it takes servos to move to next position range [MIN_MOVE_TIME, MAX_MOVE_TIME]
+  #define MAX_MOVE_TIME 50
+  ```
+  - Save and re-upload! Once you're satisfied, you can disconnect the board from your computer and power the laser tower from any outlet.
+
+
+### Happy Ninja-Cat Training!
+![Wiring Diagram](pictures/NinjaCat_1.jpg)
+![Wiring Diagram](pictures/NinjaCat_2.jpg)
--- laserTower.ino
+++ laserTower.ino
@@ -1,12 +1,12 @@
 /*
    laserTower.ino
-   
-   Ninja Cat - a robot laser-tower toy that randomly moves a servo tower on the X and Y axis 
+
+   Ninja Cat - a robot laser-tower toy that randomly moves a servo tower on the X and Y axis
    and lights up an attached laser to train your cat to be a ninja.
-   
+
    HOW IT WORKS:
-   The program loops, randomly choosing an (X,Y) position for the laser to point within  
-   a defined boundary (play space). New positions are checked to be greater than "MIN_MOVE" 
+   The program loops, randomly choosing an (X,Y) position for the laser to point within
+   a defined boundary (play space). New positions are checked to be greater than "MIN_MOVE"
    from the old position and the laser stays still for a random time between "MIN_FREEZE"
    and "MAX_FREEZE" to simulate a real bug flying around and landing.
 
@@ -17,16 +17,14 @@
 
 /* MODIFY THESE IF YOU WANT */
 
-
-
  #define MIN_X 10        // X-servo angle range [MIN_X, MAX_X]
- #define MAX_X 90      
+ #define MAX_X 90
  #define MIN_Y 0         // Y-servo angle range [MIN_Y, MAX_Y]
  #define MAX_Y 40
  #define MIN_MOVE 10      // smallest distance allowed for a new location from the current position
  #define MIN_FREEZE 100   // time to wait until next move
  #define MAX_FREEZE 1500
- #define MIN_MOVE_TIME 20 // time it takes the servos to move to new position 
+ #define MIN_MOVE_TIME 20 // time it takes the servos to move to new position
  #define MAX_MOVE_TIME 50
 
 /* CODE BELOW THIS LINE SHOULDNT NEED MODIFICATION */
@@ -56,7 +54,7 @@ void setup(){
 
   //Write starting position to servos
   X_Servo.write(x_pos);
-  Y_Servo.write(y_pos);  
+  Y_Servo.write(y_pos);
 }
 
 void loop(){
@@ -65,20 +63,20 @@ void loop(){
   x_new = random(MIN_X + MIN_MOVE, MAX_X - MIN_MOVE);
   y_new = random(MIN_Y + MIN_MOVE, MAX_Y - MIN_MOVE);
 
-//checks that abs(x_new) is greater than MIN_MOVE. If not, add or subtract MIN_MOVE 
+//checks that abs(x_new) is greater than MIN_MOVE. If not, add or subtract MIN_MOVE
   if(abs(x_new - x_old) < MIN_MOVE){
     if(x_new > x_old)
       x_new += MIN_MOVE;
     else if(x_new < x_old)
-      x_new -= MIN_MOVE;  
+      x_new -= MIN_MOVE;
   }
 
-//checks that abs(y_new) is greater than MIN_MOVE. If not, add or subtract MIN_MOVE 
+//checks that abs(y_new) is greater than MIN_MOVE. If not, add or subtract MIN_MOVE
   if(abs(y_new - y_old) < MIN_MOVE){
     if(y_new > y_old)
       y_new += MIN_MOVE;
     else if(y_new < y_old)
-      y_new -= MIN_MOVE;  
+      y_new -= MIN_MOVE;
   }
 
   //divide distance by move_time to determine a "velocity"
@@ -86,23 +84,20 @@ void loop(){
   y_velocity = (y_new - y_old) / move_time;
 
   //increment position by "velocity"
-  for (i=0; i < move_time; i++) { 
+  for (i=0; i < move_time; i++) {
       x_pos += x_velocity;
       y_pos += y_velocity;
-      X_Servo.write(x_pos);  
-      Y_Servo.write(y_pos);                    
-      delay(10); 
+      X_Servo.write(x_pos);
+      Y_Servo.write(y_pos);
+      delay(10);
   }
 
-  //updates old positions 
+  //updates old positions
   x_old = x_new;
   y_old = y_new;
 
   //delay beofore moving to next position
   delay(rand_freeze);
-  
+
 
 }
-
- 
-   
